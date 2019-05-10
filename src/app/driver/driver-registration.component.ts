3206177eea9c580b9683c16c2c1ab616903a64ca
Todkar <mrunal-sunilkumar.todkar@capgemini.com>
2019-05-10 19:20:54 +0530

    driver and admin signin and signup service done

---
+++ src/app/driver/driver-registration.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import{Driver} from './driver';
+import {DriverService} from './driver.service';
+import { enableProdMode } from '@angular/core';
+import { Router } from '@angular/router';
+
+enableProdMode();
+
+@Component({
+    templateUrl:"./driver-registration.component.html"
+    //selector:"driver-registration"
+})
+
+export class DriverRegistrationComponent implements OnInit{
+
+    driver:Driver;
+    constructor(private driverService: DriverService, private router: Router){}
+    ngOnInit(){
+        this.driver=new Driver();
+    }
+
+    registrationForDriver(){
+        console.log(this.driver);
+        this.driverService.registrationForDriver(this.driver).subscribe((data)=>{
+            console.log("success");
+            console.log(data);
+            if(data!=null){
+                alert("Done");
+                this.router.navigate(["/driver/login"]);
+            }
+        })
+    }
+
+
+}
---
+++ src/app/user/user.ts
@@ -0,0 +1,8 @@
+export class User{
+    UserRideId:number;
+    pickUpLocation:string;
+    dropLocation:string;
+    rideFare:number;
+    rideDate:Date;
+}
+
